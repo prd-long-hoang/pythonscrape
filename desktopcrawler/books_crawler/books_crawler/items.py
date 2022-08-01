6a929bcead32bf86d307e263a50e8565d26d7b56
prd-long-hoang <104879458+prd-long-hoang@users.noreply.github.com>
2022-08-01 16:45:21 +0700

    Add More Works

--- desktopcrawler/books_crawler/books_crawler/items.py
+++ desktopcrawler/books_crawler/books_crawler/items.py
@@ -9,4 +9,6 @@ import scrapy
 class BooksCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
     pass
--- desktopcrawler/books_crawler/books_crawler/spiders/books.py
+++ desktopcrawler/books_crawler/books_crawler/spiders/books.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
+from time import sleep
+from books_crawler.items import BooksCrawlerItem
 from scrapy import Spider
 from selenium import webdriver
 from scrapy.selector import Selector
 from scrapy.http import Request
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BooksSpider(Spider):
@@ -19,5 +21,29 @@ class BooksSpider(Spider):
             url = 'http://books.toscrape.com/' + book
             yield Request(url, callback=self.parse_book)
 
+        while True:
+            try:
+                next_page = self.driver.find_element("xpath",'//a[text()="next"]')
+                sleep(3)
+                self.logger.info('Sleeping for 3 seconds.')
+                next_page.click()
+
+                sel = Selector(text=self.driver.page_source)
+                books = sel.xpath('//h3/a/@href').extract()
+                for book in books:
+                    url = 'http://books.toscrape.com/catalogue/' + book
+                    yield Request(url, callback=self.parse_book)
+
+            except NoSuchElementException:
+                self.logger.info('No more pages to load.')
+                self.driver.quit()
+                break
+
     def parse_book(self, response):
-        pass
+        items = BooksCrawlerItem()
+        title = response.css('h1::text').extract_first()
+        url = response.request.url
+    
+        items['title'] = title
+        items['url'] = url
+        yield items
